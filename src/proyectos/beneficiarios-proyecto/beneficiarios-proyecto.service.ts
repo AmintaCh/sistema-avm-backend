52f92f03eb50fb011619104d359819b84e890278
Aminta <aminta.mendezch@gmail.com>
2025-10-30 02:24:03 -0600

    se modificó servicio para listar beneficiarios de un proyecto

--- src/proyectos/beneficiarios-proyecto/beneficiarios-proyecto.service.ts
+++ src/proyectos/beneficiarios-proyecto/beneficiarios-proyecto.service.ts
@@ -73,7 +73,7 @@ export class ProyectosBeneficiariosService {
       .addSelect('p.persona_id', 'personaId')
       .addSelect('p.primer_nombre', 'primerNombre')
       .addSelect('p.segundo_nombre', 'segundoNombre')
-      .addSelect('p.tercer_nombre', 'tercerNomber')
+      .addSelect('p.tercer_nombre', 'tercerNombre')
       .addSelect('p.primer_apellido', 'primerApellido')
       .addSelect('p.segundo_apellido', 'segundoApellido')
       .addSelect('bp.fecha_incorporacion', 'fechaIncorporacion')
@@ -90,7 +90,7 @@ export class ProyectosBeneficiariosService {
         ? { personaId: r.personaId,
           primerNombre: r.primerNombre,
           segundoNombre:r.segundoNombre,
-          tercerNomber:r.tercerNomber,
+          tercerNombre:r.tercerNombre,
           primerApellido: r.primerApellido,
           segundoApellido: r.segundoApellido
         }
